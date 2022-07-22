7457b50f2a4cbbb928cf77e9a9e70ede25b8e66e
jacoposergio <djago32@gmail.com>
2022-07-22 15:28:24 +0200

    first commit

---
+++ snack-1/index.php
@@ -0,0 +1,59 @@
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra 
+ di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. -->
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+ <?php
+
+$matches = [
+    [
+        'casa' => 'Maverick',
+        'trasferta' => 'Huston',
+        'puntiCasa' => 108,
+        'puntitrasferta' => 112
+    ],
+    [
+        'casa' => 'Maverick',
+        'trasferta' => 'Huston',
+        'puntiCasa' => 97,
+        'puntitrasferta' => 99
+    ],
+    [
+        'casa' => 'Clippers',
+        'trasferta' => 'San Antonio',
+        'puntiCasa' => 103,
+        'puntitrasferta' => 125
+    ],
+    [
+        'casa' => 'Bucks',
+        'trasferta' => 'Brooklyn',
+        'puntiCasa' => 76,
+        'puntitrasferta' => 102
+    ],
+    [
+        'casa' => 'Pistons',
+        'trasferta' => 'Sacramento',
+        'puntiCasa' => 97,
+        'puntitrasferta' => 92
+    ],
+];
+
+// var_dump($matches[4]);
+?>
+
+<h2>Partite di ieri</h2>
+<ul>
+  <li><?php       ?></li>
+</ul>
+
+ </body>
+ </html>
